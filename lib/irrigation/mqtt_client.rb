cb7dcc6ea7da8b14d0a1376caf90d3ad930e0ded
Sean McCarthy <sean@clanmccarthy.net>
2017-02-12 22:08:47 +1100

    Adding reconnect to mqtt client wrapper

--- lib/irrigation/mqtt_client.rb
+++ lib/irrigation/mqtt_client.rb
@@ -5,16 +5,30 @@ module Irrigation
     attr_reader :host, :port
 
     def initialize(opts)
-      @host = opts.fetch(:host) { 'localhost' }
+      @host = opts.fetch(:host, 'localhost')
+      @port = opts.fetch(:port, 1883)
     end
 
     def send_command(station, command)
-      @client.publish("command/#{station.id}", command)
+      client.publish("command/#{station}", command)
     end
 
     def get_status(&block)
-      @client.subscribe('status/#')
-      @client.get &block
+      client.subscribe('status/#')
+      client.get &block
+    end
+
+    private
+
+    def client
+      reconnect unless @client && @client.connected?
+      @client
+    end
+
+    def reconnect
+      @client = MQTT::Client.connect(:host => @host, :port => @port)
+    rescue => e
+      # log ?
     end
   end
 end
