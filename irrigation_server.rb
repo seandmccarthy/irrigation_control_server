6d810226621fb3e996895a13cfb70a514f871aef
Sean McCarthy <sean@clanmccarthy.net>
2017-03-03 21:27:34 +1100

    Wrap setup/configuration of main controller class in a method for the Sinatra module

--- irrigation_server.rb
+++ irrigation_server.rb
@@ -1,29 +1,42 @@
 require 'sinatra/base'
 require 'mqtt'
 require 'json'
+require 'irrigation'
 
 class IrrigationServer < Sinatra::Base
   set :public_folder, File.dirname(__FILE__) + '/public'
+  set :server, :thin
+  set :connections, []
 
-  configure do
-    set :station_ids, %w(1 2 3 4)
-    set :mqtt_broker, 'localhost' # '10.0.1.6'
+  def self.init_controller
+    Irrigation::Controller.new(
+      stations: [
+        Irrigation::Station.new(id: 1, description: 'Veggie Garden'),
+        Irrigation::Station.new(id: 2, description: 'Backyard Garden'),
+        Irrigation::Station.new(id: 3, description: 'Front Garden'),
+        Irrigation::Station.new(id: 4, description: 'Japanese Garden')
+      ],
+      message_client: Irrigation::MqttClient.new(host: '10.0.1.202')
+    )
   end
 
-  def station_data(id)
-    {
-      id: id,
-      description: "Station #{id}",
-      state: station_status(id)
-    }
+  configure do
+    set :controller, init_controller
+    settings.controller.start
   end
 
   get '/' do
     send_file 'public/index.html'
   end
 
+  get '/status' do
+    content_type :json
+    settings.controller.stations.to_json
+  end
+
   post '/stations/:id' do
-    "got: #{params["command"]}"
+    settings.controller.send_command(params[:id], params[:command])
+    :ok
   end
 
   run! if app_file == $0
